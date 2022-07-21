ef322c755d81f61b4593c385c2f0a6a0bd49762c
Aldss <aldongmolano08@gmail.com>
2022-07-21 16:25:04 +0800

    Up1 Plantor App

--- app/src/main/java/com/example/plantor/NotificationReciever1.java
+++ app/src/main/java/com/example/plantor/NotificationReciever1.java
@@ -16,31 +16,61 @@ import androidx.core.app.NotificationManagerCompat;
 public class NotificationReciever1 extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        int notificationId = 1;
-        String channelId = "channel-01";
-        String channelName = "Channel Name";
-        int importance = NotificationManager.IMPORTANCE_HIGH;
+       if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")){
+           NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+           int notificationId = 1;
+           String channelId = "channel-01";
+           String channelName = "Channel Name";
+           int importance = NotificationManager.IMPORTANCE_HIGH;
 
-        Intent repeating_intent = new Intent(context, MainActivity.class);
-        repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel mChannel = new NotificationChannel(
-                    channelId, channelName, importance);
-            notificationManager.createNotificationChannel(mChannel);
-        }
+           Intent repeating_intent = new Intent(context, MainActivity.class);
+           repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+           if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+               NotificationChannel mChannel = new NotificationChannel(
+                       channelId, channelName, importance);
+               notificationManager.createNotificationChannel(mChannel);
+           }
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
-                .setSmallIcon(android.R.drawable.arrow_up_float)
-                .setContentTitle("Your Plant")
-                .setContentText("Check fertilizer!")
-                .setAutoCancel(true);
+           NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
+                   .setSmallIcon(android.R.drawable.arrow_up_float)
+                   .setContentTitle("Your Plant")
+                   .setContentText("Check fertilizer!")
+                   .setAutoCancel(true);
 
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addNextIntent(repeating_intent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(resultPendingIntent);
+           TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+           stackBuilder.addNextIntent(repeating_intent);
+           PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
+           mBuilder.setContentIntent(resultPendingIntent);
 
-        notificationManager.notify(notificationId, mBuilder.build());
+           notificationManager.notify(notificationId, mBuilder.build());
+       }else{
+           NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+           int notificationId = 1;
+           String channelId = "channel-01";
+           String channelName = "Channel Name";
+           int importance = NotificationManager.IMPORTANCE_HIGH;
+
+           Intent repeating_intent = new Intent(context, MainActivity.class);
+           repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+           if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+               NotificationChannel mChannel = new NotificationChannel(
+                       channelId, channelName, importance);
+               notificationManager.createNotificationChannel(mChannel);
+           }
+
+           NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, channelId)
+                   .setSmallIcon(android.R.drawable.arrow_up_float)
+                   .setContentTitle("Your Plant")
+                   .setContentText("Check fertilizer!")
+                   .setAutoCancel(true);
+
+           TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+           stackBuilder.addNextIntent(repeating_intent);
+           PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
+           mBuilder.setContentIntent(resultPendingIntent);
+
+           notificationManager.notify(notificationId, mBuilder.build());
+       }
+
     }
 }
